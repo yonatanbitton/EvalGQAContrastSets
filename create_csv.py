61431b70e01bba6379a7a144f939a378afb746a7
Yonatan Bitton <yonatab@amazon.com>
2020-10-19 18:13:22 +0300

    commit

--- create_csv.py
+++ create_csv.py
@@ -9,10 +9,12 @@ def main():
     all_dataframes = [pd.read_csv(os.path.join(mTurk_dir, x)) for x in csv_files]
     all_dataframes_merged = pd.concat(all_dataframes)
     all_dataframes_merged = all_dataframes_merged.sample(frac=1)
-    all_dataframes_merged['image_url'] = all_dataframes_merged['img_id'].apply(lambda x: f"https://github.com/yonatanbitton/EvalGQAContrastSets/blob/main/images/{x}.png")
+    all_dataframes_merged['image_url'] = all_dataframes_merged['img_id'].apply(lambda x: f"https://github.com/yonatanbitton/EvalGQAContrastSets/blob/main/images/{x}.png?raw=true")
     all_dataframes_merged.drop(columns=['Unnamed: 0'], inplace=True)
     all_dataframes_merged.to_csv(os.path.join(mTurk_dir, 'turks_data.csv'), index=False)
     batches_dir = os.path.join(mTurk_dir, 'batches')
+    if not os.path.exists(batches_dir):
+        os.mkdir(batches_dir)
     first_batch = all_dataframes_merged.iloc[0:500]
     first_batch.to_csv(os.path.join(batches_dir, 'turks_data_0_500.csv'), index=False)
     second_batch = all_dataframes_merged.iloc[500:1000]
